00e72bd85eff158bc3f16067841a53c7e29eaaed
Colman Currie <curriecolman@gmail.com>
2020-05-20 00:09:48 -0700

    create backend node.js and push to repo

---
+++ src/build/precache-manifest.2cba9814670f8f573fecbbc3bc78a012.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b0869930fd6aab57a51a7c75537f555",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfa5c968a79d68b55a1c",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "fc25cc397cf10e67e720",
+    "url": "/static/js/2.cb466dbd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cb466dbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfa5c968a79d68b55a1c",
+    "url": "/static/js/main.b2316c34.chunk.js"
+  },
+  {
+    "revision": "8d3045021bf6a331c390",
+    "url": "/static/js/runtime-main.4d19937a.js"
+  }
+]);
